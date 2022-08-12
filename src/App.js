c2264997aef7c9c602378e1d8f895838ba581ce4
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-08-12 14:29:18 +0300

    static html and css moved to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header>
+          <ul>
+            <div className="left">
+              <li><a Link="">Каса</a></li>
+              <li><a Link="">Съобщения</a></li>
+              <li><a Link="">Анкети</a></li>
+            </div>
+            <div className="right">
+              <li><a id="greeting" Link="">Здравей, Иван Иванов</a></li>
+              <li><a id="loginBtn" Link="">вход</a></li>
+              <li><a id="registerBtn" Link="">регистрация</a></li>
+              <li><a id="logoutBtn" Link="">изход</a></li>
+            </div>
+          </ul>
+        </header>
+
+        <div className="home-page">
+          <main>
+
+
+            <section className="table">
+
+              <table>
+                <tr>
+                  <th>Ап</th>
+                  <th>Семейство</th>
+                  <th>брой живущи</th>
+                  <th>такса</th>
+                  <th>Януари</th>
+                </tr>
+                <tr>
+                  <td>89</td>
+                  <td>Иванови</td>
+                  <td>3</td>
+                  <td>24</td>
+                  <td>платено</td>
+                </tr>
+                <tr>
+                  <td>89</td>
+                  <td>Иванови</td>
+                  <td>3</td>
+                  <td>24</td>
+                  <td>платено</td>
+                </tr>
+                <tr>
+                  <td>89</td>
+                  <td>Иванови</td>
+                  <td>3</td>
+                  <td>24</td>
+                  <td>платено</td>
+                </tr>
+                <tr>
+                  <td>89</td>
+                  <td>Иванови</td>
+                  <td>3</td>
+                  <td>24</td>
+                  <td>платено</td>
+                </tr>
+
+              </table>
+            </section>
+
+            <aside>
+              <section className="prihodi">
+                <h2>Приходи</h2>
+                <p>100лв</p>
+              </section>
+
+              <section className="razhodi">
+                <h2>Разходи</h2>
+                <p>100лв</p>
+              </section>
+
+              <section className="nalichnost">
+                <h2>Текуща наличност</h2>
+                <p>100лв</p>
+              </section>
+            </aside>
+
+
+          </main>
+        </div>
+        <section className="register-page">
+          <form action="">
+
+            <div className="email-input">
+              <label>E-mail:</label>
+              <input type="text" />
+            </div>
+
+            <div className="name-input">
+              <label>Име:</label>
+              <input type="text" />
+            </div>
+
+            <div className="last-name-input">
+              <label>Фамилия:</label>
+              <input type="text" />
+            </div>
+
+            <div className="apartment-input">
+              <label>Живущ в апартамент: </label>
+              <input type="text" />
+            </div>
+
+            <div className="password">
+              <label>Парола:</label>
+              <input type="password" />
+            </div>
+
+            <div className="re-pass">
+              <label>Повтори паролата:</label>
+              <input type="password" />
+            </div>
+
+            <input className="submit-btn" type="submit" value="Създай профил" />
+
+          </form>
+        </section>
+
+        <section className="login-page">
+          <form action="">
+
+            <div className="email-input">
+              <label>E-mail:</label>
+              <input type="text" />
+            </div>
+
+            <div className="password">
+              <label>Парола:</label>
+              <input type="password" />
+            </div>
+
+            <input className="submit-btn" type="submit" value="Създай профил" />
+
+            <div class ="key-picture">
+            <img src="./images/key2.png" alt="" />
+            </div>
+          </form>
+
+        </section>
+      </body>
     </div>
   );
 }
