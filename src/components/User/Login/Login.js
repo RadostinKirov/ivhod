8aa645f5089876823f0f263232b219be6334a358
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-08-13 18:22:12 +0300

    Login basic validation
    check if input fields are empty

--- src/components/User/Login/Login.js
+++ src/components/User/Login/Login.js
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
+const [email, setEmail] = useState('');
+const [password, setPassword] =useState('');
+
+const onChangeEmailHandler = (e) => {
+  let emailInput = e.target.value;
+  setEmail(emailInput);
+}
+
+const onChangePasswordHandler = (e) => {
+  let passwordInput = e.target.value;
+  setPassword(passwordInput);
+}
+
+const onSubmitHandler = (e) => {
+  e.preventDefault();
+  if(!email){
+    console.log('Моля въведи email!');
+  }
+
+  if(!password){
+    console.log('Моля въведи парола!')
+  }
+}
 
     return (
         
         <section className="login-page">
-          <form action="">
+          <form action="" onSubmit={onSubmitHandler}>
 
             <div className="email-input">
               <label>E-mail:</label>
-              <input type="text" />
+              <input onChange={onChangeEmailHandler} value={email} type="text" />
             </div>
 
             <div className="password">
               <label>Парола:</label>
-              <input type="password" />
+              <input onChange={onChangePasswordHandler} value={password} type="password" />
             </div>
 
             <input className="submit-btn" type="submit" value="Създай профил" />
