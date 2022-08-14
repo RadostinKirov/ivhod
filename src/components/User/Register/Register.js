63e0c3db653df1f153c262cdbec8212ff8d2269a
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-08-14 16:39:26 +0300

    Register Form - input fields handlers added

--- src/components/User/Register/Register.js
+++ src/components/User/Register/Register.js
@@ -1,48 +1,87 @@
+import { useState } from 'react';
 import './Register.css';
 
 const Register = () => {
 
-    return (
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [appratment, setAppartment] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePass, setRePass] = useState('');
 
-        <section className="register-page">
-          <form action="">
 
-            <div className="email-input">
-              <label>E-mail:</label>
-              <input type="text" />
-            </div>
+  const onChangeEmailHandler = (e) => {
+    let emailInput = e.target.value;
+    setEmail(emailInput);
+  }
 
-            <div className="name-input">
-              <label>Име:</label>
-              <input type="text" />
-            </div>
+  const onChangeNameHandler = (e) => {
+    let nameInput = e.target.value;
+    setName(nameInput);
+  }
 
-            <div className="last-name-input">
-              <label>Фамилия:</label>
-              <input type="text" />
-            </div>
+  const onChangeLastNameHandler = (e) => {
+    let lastNameInput = e.target.value;
+    setLastName(lastNameInput);
+  }
 
-            <div className="apartment-input">
-              <label>Живущ в апартамент: </label>
-              <input type="text" />
-            </div>
+  const onChangeAppartmentHandler = (e) => {
+    let appartmentInput = e.target.value;
+    setAppartment(appartmentInput);
+  }
 
-            <div className="password-input">
-              <label>Парола:</label>
-              <input type="password" />
-            </div>
+  const onPasswordHandler = (e) => {
+    let passwordInput = e.target.value;
+    setPassword(passwordInput);
+  }
 
-            <div className="re-pass">
-              <label>Повтори паролата:</label>
-              <input type="password" />
-            </div>
+  const onRePassHandler = (e) => {
+    let rePassInput = e.target.value;
+    setRePass(rePassInput);
+  }
 
-            <input className="submit-btn" type="submit" value="Създай профил" />
+  return (
 
-          </form>
-        </section>
+    <section className="register-page">
+      <form action="">
 
-    )
+        <div className="email-input" onChange={onChangeEmailHandler}>
+          <label>E-mail:</label>
+          <input type="text" />
+        </div>
+
+        <div className="name-input" onChange={onChangeNameHandler}>
+          <label>Име:</label>
+          <input type="text" />
+        </div>
+
+        <div className="last-name-input" onChange={onChangeLastNameHandler}>
+          <label>Фамилия:</label>
+          <input type="text" />
+        </div>
+
+        <div className="apartment-input" onChange={onChangeAppartmentHandler} >
+          <label>Живущ в апартамент: </label>
+          <input type="text" />
+        </div>
+
+        <div className="password-input" onChange={onPasswordHandler} >
+          <label>Парола:</label>
+          <input type="password" />
+        </div>
+
+        <div className="re-pass" onChange={onRePassHandler}>
+          <label>Повтори паролата:</label>
+          <input type="password" />
+        </div>
+
+        <input className="submit-btn" type="submit" value="Създай профил" />
+
+      </form>
+    </section>
+
+  )
 }
 
 export default Register;
